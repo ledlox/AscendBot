895a1ba7caae4883808f8c0e810440cb08bb44fe
ledlox <jasonlutgens@gmail.com>
2026-05-15 19:10:19 +0200

    Switch to DiscordDevTools welcome API

--- utils/welcome.js
+++ utils/welcome.js
@@ -1,9 +1,15 @@
+function mapHex(color) {
+  return color.replace('#', '');
+}
+
 async function generateWelcomeUrl(member) {
   const username = encodeURIComponent(member.user.username);
-  const guildName = encodeURIComponent(member.guild.name);
+  const discriminator = member.user.discriminator === '0' ? '0000' : member.user.discriminator;
   const avatar = member.user.displayAvatarURL({ extension: 'png', size: 256 });
+  const title = encodeURIComponent('Welcome to AscendGems!');
+  const text = encodeURIComponent(`Member #${member.guild.memberCount}`);
 
-  return `https://api.some-random-api.com/welcome/img/2/4?type=join&avatar=${avatar}&username=${username}&textcolor=purple&guildName=${guildName}&memberCount=${member.guild.memberCount}`;
+  return `https://api.discorddevtools.xyz/welcome-image-generator/generate.png?title=${title}&titleColor=aa44ff&username=${username}&usernameColor=ffffff&discriminator=${discriminator}&discriminatorColor=a9a9a9&text=${text}&textColor=33ffff&image=${avatar}&borderColor=aa44ff&allCaps=false&cacheImage=true`;
 }
 
 module.exports = { generateWelcomeUrl };
